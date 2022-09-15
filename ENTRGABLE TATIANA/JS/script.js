a54ff913c388650cd0eff92f3a39ddb4a69d4bdf
castilloTati <110948497+castilloTati@users.noreply.github.com>
2022-09-14 22:48:29 -0500

    Entregable 2

---
+++ ENTRGABLE TATIANA/JS/script.js
@@ -0,0 +1,17 @@
+let button = document.querySelector("#calcular");
+button.onclick = function()
+{
+    let valor = 50000;
+    let iva = 0.16;
+    let total = 0;
+    let valor_iva = 0;
+    let valor_neto = 0;
+    let select = document.querySelector("#cantidad");
+    total = valor * parseInt(select.value);
+    valor_iva = valor * iva;
+    valor_neto = total + valor_iva;
+
+    document.querySelector('#total').innerHTML = total;
+    document.querySelector('#iva').innerHTML = valor_iva;
+    document.querySelector('#neto').innerHTML = valor_neto;
+}
